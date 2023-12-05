ac47e565dfe99e8b5267c83cc471ad533afb445f
Gus <august.colonna@gmail.com>
2023-12-05 15:35:45 +0100

    added filtering

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 //routing
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from "react-router-dom";
 //pages
-import Welcome from './pages/Welcome';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import Information from './pages/Information';
-import Workout from './pages/Workout';
+import Welcome from "./pages/Welcome";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Information from "./pages/Information";
+import Workout from "./pages/Workout";
 //components
-import FloatingNav from './components/FloatingNav';
+import FloatingNav from "./components/FloatingNav";
 //utilities
 
 //hooks
-import { useAuthContext } from './hooks/useAuthContext';
+import { useAuthContext } from "./hooks/useAuthContext";
 //styles
-import './stylesheets/index.scss';
-import CreateWorkout from './pages/CreateWorkout';
-import Ftiness from './pages/Ftiness';
+import "./stylesheets/index.scss";
+import CreateWorkout from "./pages/CreateWorkout";
+import Ftiness from "./pages/Ftiness";
 
 function App() {
   const { user, authIsReady } = useAuthContext();
@@ -26,15 +26,42 @@ function App() {
     <div className="App">
       {authIsReady && (
         <Routes>
-          <Route path="/" element={user ? <Navigate to="/home" /> : <Welcome />} />
-          <Route path="/signup" element={user ? <Navigate to="/home" /> : <Signup />} />
-          <Route path="/login" element={user ? <Navigate to="/home" /> : <Login />} />
-          <Route path="/home" element={!user ? <Navigate to="/" /> : <Home />} />
-          <Route path="/profile/:id" element={!user ? <Navigate to="/" /> : <Profile />} />
-          <Route path="/information" element={!user || (user && <Information />)} />
-          <Route path="/create-workout" element={user ? <CreateWorkout /> : <Welcome />} />
-          <Route path="/fitness" element={!user ? <Navigate to="/" /> : <Ftiness />} />
-          <Route path="/fitness/:id" element={!user ? <Navigate to="/" /> : <Workout />} />
+          <Route
+            path="/"
+            element={user ? <Navigate to="/home" /> : <Welcome />}
+          />
+          <Route
+            path="/signup"
+            element={user ? <Navigate to="/home" /> : <Signup />}
+          />
+          <Route
+            path="/login"
+            element={user ? <Navigate to="/home" /> : <Login />}
+          />
+          <Route
+            path="/home"
+            element={!user ? <Navigate to="/" /> : <Home />}
+          />
+          <Route
+            path="/profile/:id"
+            element={!user ? <Navigate to="/" /> : <Profile />}
+          />
+          <Route
+            path="/information"
+            element={!user || (user && <Information />)}
+          />
+          <Route
+            path="/create-workout"
+            element={user ? <CreateWorkout /> : <Welcome />}
+          />
+          <Route
+            path="/fitness"
+            element={!user ? <Navigate to="/" /> : <Ftiness />}
+          />
+          <Route
+            path="/fitness/:id"
+            element={!user ? <Navigate to="/" /> : <Workout />}
+          />
         </Routes>
       )}
       {user && <FloatingNav />}
--- src/pages/Ftiness.jsx
+++ src/pages/Ftiness.jsx
@@ -1,17 +1,17 @@
 //hooks
-import { useCollection } from '../hooks/useCollection';
-import { useState } from 'react';
-import { useAuthContext } from '../hooks/useAuthContext';
+import { useCollection } from "../hooks/useCollection";
+import { useState } from "react";
+import { useAuthContext } from "../hooks/useAuthContext";
 //components
-import WorkoutList from '../components/WorkoutList';
-import WorkoutFilter from '../components/WorkoutFilter';
-import { Link } from 'react-router-dom';
+import WorkoutList from "../components/WorkoutList";
+import WorkoutFilter from "../components/WorkoutFilter";
+import { Link } from "react-router-dom";
 //styles
-import '../stylesheets/fitness.scss';
+import "../stylesheets/fitness.scss";
 
 function Ftiness() {
-  const [scheduleFilter, setScheduleFilter] = useState('all');
-  const { documents, error } = useCollection('workouts');
+  const [scheduleFilter, setScheduleFilter] = useState("all");
+  const { documents, error } = useCollection("workouts");
   const { user } = useAuthContext();
   const changeFilter = (newfilter) => {
     setScheduleFilter(newfilter);
@@ -20,27 +20,44 @@ function Ftiness() {
   const filterByDay = documents
     ? documents.filter((document) => {
         switch (scheduleFilter) {
-          case 'all': {
-            let isCurrent = documents
-              .sort((a, b) => {
-                return b.date - a.date;
-              })
-              .filter((document) => {
-                let today = new Date().toLocaleDateString('de-DE');
-                if (document.date.toDate().toLocaleDateString('de-DE') >= today) {
-                  return document;
-                }
-              });
-
+          case "all": {
+            if (
+              document.date.toDate().toLocaleDateString() <
+              new Date().toLocaleDateString()
+            ) {
+              return !document;
+            }
+            let isCurrent = documents.sort((a, b) => {
+              return b.date - a.date;
+            });
             return isCurrent;
           }
-          case 'today': {
-            let isToday = false;
-            let today = new Date().toLocaleDateString('de-DE');
-            if (today === document.date.toDate().toLocaleDateString('de-DE')) {
-              isToday = true;
+          case "today": {
+            if (
+              new Date().toLocaleDateString() ==
+              document.date.toDate().toLocaleDateString()
+            ) {
+              return document;
             }
-            return isToday;
+            break;
+          }
+          case "mine": {
+            document.signUpList.filter((client) => {
+              if (client.id == user.uid) {
+                return document;
+              }
+            });
+            if (
+              document.date.toDate().toLocaleDateString() <
+              new Date().toLocaleDateString()
+            ) {
+              return !document;
+            }
+            documents.sort((a, b) => {
+              return b.date - a.date;
+            });
+
+            break;
           }
           default:
             return true;
@@ -51,12 +68,17 @@ function Ftiness() {
   return (
     <div className="fitness-container">
       {error && <p>{error}</p>}
-      {documents && <WorkoutFilter changeFilter={changeFilter} scheduleFilter={scheduleFilter} />}
-      {user.isCoach && (
-        <Link to="/create-workout" className="create-workout-link">
-          <button className="btn">Create Workout</button>
-        </Link>
+      {documents && (
+        <WorkoutFilter
+          changeFilter={changeFilter}
+          scheduleFilter={scheduleFilter}
+        />
       )}
+
+      <Link to="/create-workout" className="create-workout-link">
+        <button className="btn">Create New Workout</button>
+      </Link>
+
       {filterByDay && <WorkoutList workouts={filterByDay} />}
     </div>
   );
